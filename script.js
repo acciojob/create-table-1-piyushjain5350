6b30cc581713cc35bb45ea616a5cd37b49d65bb3
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-26 16:11:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const tableContent=document.querySelector("#sampleTable");
+let cellNo=1;
 function insert_Row() {
     //Write your code here
-  
-  
+	const tr=document.createElement("tr");
+	let altCellNo=cellNo+1;
+	tr.innerHTML=`
+		<td>New Cell ${cellNo}</td> 
+		<td>New Cell ${altCellNo}</td>
+ `
+  tableContent.prepend(tr);
+	cellNo+=2;
 }
